f16bf8964b9ea465f33144e091eac1c06e80edb4
劉品均 <encoreu63990@gmail.com>
2022-04-24 23:07:48 +0800

    加入專案檔案。

---
+++ Practice_Core6Book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practice_Core6Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
