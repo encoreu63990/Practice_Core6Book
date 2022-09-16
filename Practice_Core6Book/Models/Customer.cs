b11e835dfd19c3d9f7544390dcdf084865c2eea5
劉品均 <encoreu63990@gmail.com>
2022-09-17 01:52:48 +0800

    update customer model

--- Practice_Core6Book/Models/Customer.cs
+++ Practice_Core6Book/Models/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public string Account { get; set; }
-        public string Password { get; set; }
+        public string Account { get; set; } = "";
+        public string Password { get; set; } = "";
     }
 }
